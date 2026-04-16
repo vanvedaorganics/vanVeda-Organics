1df7b0573bc150fb143cb6bbc79141654d761c37
VED PATEL <115972549+Brooklyiner@users.noreply.github.com>
2026-04-16 11:14:20 +0530

    feat: add getImageUrl utility and placeholder SVG for Appwrite storage files

--- utils/getImageUrl.js
+++ utils/getImageUrl.js
@@ -4,6 +4,6 @@ import conf from "../src/conf/conf";
 export const getImageUrl = (fileId) => {
   if (!fileId) return "/placeholder.svg";
 
-  // Use /view endpoint instead of /preview for free plan
-  return `${conf.appwriteUrl}/storage/buckets/${conf.appwriteBucketId}/files/${fileId}/view?project=${conf.appwriteProjectId}`;
+  // Use /preview for optimized web display and better permission compatibility
+  return `${conf.appwriteUrl}/storage/buckets/${conf.appwriteBucketId}/files/${fileId}/preview?project=${conf.appwriteProjectId}`;
 };
